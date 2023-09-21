4086648f1942fe410926021c34d03e1e81b42d2e
Maxime-Le-Goff <lgmaximemlg@gmail.com>
2023-09-21 11:02:33 +0200

    setup react-vite + sections

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Hero from './Hero';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import Nav from './Nav';
+
+
+export { Hero, SuperQuality, CustomerReviews, Footer, PopularProducts, Services, SpecialOffer, Nav, Subscribe}
